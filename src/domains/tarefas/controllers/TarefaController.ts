5eba206bdf507de0c62e8753dc7e31584957c6e6
Luis Tieres <luistieres@gmail.com>
2026-08-02 12:49:53 -0300

    feat: migra Prisma e MySQL

--- src/domains/tarefas/controllers/TarefaController.ts
+++ src/domains/tarefas/controllers/TarefaController.ts
@@ -4,46 +4,46 @@ import { TarefaService } from '../services/TarefaService'
 const service = new TarefaService()
 
 export class TarefaController {
-  create(req: Request, res: Response) {
+  async create(req: Request, res: Response) {
     try {
       const { title } = req.body
-      const tarefa = service.create({ title })
+      const tarefa = await service.create({ title })
       return res.status(201).json(tarefa)
     } catch (error: any) {
       return res.status(400).json({ erro: error.message })
     }
   }
 
-  list(req: Request, res: Response) {
-    const tarefas = service.list()
+  async list(req: Request, res: Response) {
+    const tarefas = await service.getAll()
     return res.status(200).json(tarefas)
   }
 
-  findById(req: Request, res: Response) {
+  async findById(req: Request, res: Response) {
     try {
-      const { id } = req.params
-      const tarefa = service.findById(id)
+      const id = Number(req.params.id)
+      const tarefa = await service.getById(id)
       return res.status(200).json(tarefa)
     } catch (error: any) {
       return res.status(404).json({ erro: error.message })
     }
   }
 
-  update(req: Request, res: Response) {
+  async update(req: Request, res: Response) {
     try {
-      const { id } = req.params
+      const id = Number(req.params.id)
       const { title, completed } = req.body
-      const tarefa = service.update(id, { title, completed })
+      const tarefa = await service.update(id, { title, completed })
       return res.status(200).json(tarefa)
     } catch (error: any) {
       return res.status(404).json({ erro: error.message })
     }
   }
 
-  delete(req: Request, res: Response) {
+  async delete(req: Request, res: Response) {
     try {
-      const { id } = req.params
-      service.delete(id)
+      const id = Number(req.params.id)
+      await service.delete(id)
       return res.status(204).send()
     } catch (error: any) {
       return res.status(404).json({ erro: error.message })
